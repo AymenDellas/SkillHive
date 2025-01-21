18f1d785f8efac632e63190f23dfbc4b7f9bd4b8
AymenDellas <aymen.dellas@etu.usthb.dz>
2025-01-21 14:35:51 +0100

    Speend insights

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import WhyChoose from "./components/UI-Comps/WhyChoose";
 import Courses from "./components/UI-Comps/Courses";
 import Contact from "./components/Layout-Comps/Contact";
 import { easeOut, motion } from "motion/react";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const App = () => {
   return (
     <>
+      <SpeedInsights />
       <div className="min-h-screen relative">
         <div className="absolute -z-50 inset-0  bg-background bg-[linear-gradient(to_right,#80808012_3px,transparent_3px),linear-gradient(to_bottom,#80808012_3px,transparent_3px)] bg-[size:24px_24px]" />
 
--- src/components/Layout-Comps/Contact.jsx
+++ src/components/Layout-Comps/Contact.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { motion } from "motion/react";
 import Form from "../UI-Comps/Form";
+import { div } from "framer-motion/client";
 
 const Contact = () => {
   return (
-    <div className="mx-80 my-12 max-xl:mx-8 bg-primary/50 p-8 rounded-lg shadow-2xl">
-      <Form />
+    <div className="pb-36">
+      <div className="mx-80  max-xl:mx-8 bg-primary/50 p-8 rounded-lg shadow-2xl">
+        <Form />
+      </div>
     </div>
   );
 };
